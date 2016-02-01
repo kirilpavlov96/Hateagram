e3eb6f5ce73475cc8a7e3edcd39308fe5606ab11
Teodor Marinov <T_M_Marinov@yahoo.co.uk>
2016-02-01 22:33:51 +0200

    Date added and some StringBuilders

--- week_1_OOP/src/Comment.java
+++ week_1_OOP/src/Comment.java
@@ -1,24 +1,34 @@
+import java.time.LocalDateTime;
 
 public class Comment {
-	private String text;
+	private StringBuilder text;
 	private IUser commentator;
 	private Post post;
+	private final LocalDateTime dateAdded;
 	
-	public Comment(String text,IUser user,Post post){
+	public Comment(StringBuilder text,IUser user,Post post){
 		this.setText(text);
 		this.setCommentator(user);
 		this.setPost(post);
+		this.dateAdded=LocalDateTime.now();
+	}
+	
+	public LocalDateTime getDateAdded(){
+		return this.dateAdded;
 	}
 
-	private void setText(String text) {
+	private void setText(StringBuilder text) {
+		if(text!=null)
 		this.text = text;
 	}
 
 	private void setCommentator(IUser commentator) {
+		if (commentator!=null)
 		this.commentator = commentator;
 	}
 
 	private void setPost(Post post) {
+		if(post!=null)
 		this.post = post;
 	}
 	
